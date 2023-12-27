d4f87deb877c321343f2bae419543e28f0846221
Zaid Ahmed From PC 2 <zaidahmed2345@gmail.com>
2023-12-27 20:41:33 +0500

    finally some progress

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
-const firstName = document.getElementById("fname").textContent;
-const lastName = document.getElementById("lname");
-const contact = document.getElementById("contact");
-const country = document.getElementById("country");
 const submitBtn = document.getElementById("submitbtn");
 const tbody = document.querySelector("tbody");
 
 submitBtn.addEventListener("click", (e) => {
   e.preventDefault();
-  updateTable();
+  getValue();
 });
 
-function updateTable() {
+function getValue() {
+  const firstName = document.getElementById("fname").value;
+  const lastName = document.getElementById("lname").value;
+  const contact = document.getElementById("contact").value;
+  const country = document.getElementById("country").value;
+
   let tr = document.createElement("tr");
   let td = document.createElement("td");
 
